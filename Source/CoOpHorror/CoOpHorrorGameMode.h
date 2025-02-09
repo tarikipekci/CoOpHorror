5f77173a7925575df84427d873849b0f135a4ab4
Yilmaz Tarik Ipekci <ytarik.ipekci@gmail.com>
2025-02-09 17:14:32 +0300

    Added -> Crouching and Sprinting

---
+++ Source/CoOpHorror/CoOpHorrorGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CoOpHorrorGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ACoOpHorrorGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ACoOpHorrorGameMode();
+};
+
+
+
